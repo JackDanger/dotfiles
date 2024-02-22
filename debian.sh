94a93adde0b935fa70c4421ec63d004c1d8f2362
Jack Danger <github@jackcanty.com>
2024-02-21 16:06:42 -0800

    lowercase meh

--- debian.sh
+++ debian.sh
@@ -7,7 +7,7 @@ apt install -y curl gnupg2
 curl "https://raw.githubusercontent.com/JackDanger/dotfiles/main/debian.mdns.sh" -H "Cache-Control: no-cache" | bash
 
 tee -a /etc/bash.bashrc <<-EOS
-alias l='ls -LAG'
+alias l='ls -lAG'
 alias reload='systemctl daemon-reload'
 alias start='systemctl start'
 alias stop='systemctl stop'
